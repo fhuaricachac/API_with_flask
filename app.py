bbf11022fb5d327dc660eff940d5d9d86407270a
BWIT.Huaricacha Chuco <fhuaricachac@proveedor.intercorp.com.pe>
2024-01-31 20:26:04 -0500

    API with 1000 register with only post

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import os, sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///globant.db'
@@ -22,51 +23,77 @@ class Employee(db.Model):
     department_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=True)
     job_id = db.Column(db.Integer, db.ForeignKey('job.id'), nullable=True)
 
+transactions = []
+batch_size = 1000
+
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
 @app.route('/load_data', methods=['POST'])
 def load_data():
     try:
         if not request.is_json:
             return jsonify({'error': 'Invalid JSON payload'}), 400
 
-        transactions = request.json.get('transactions')
+        data_to_send = request.json.get('files')
 
-        if not isinstance(transactions, list):
-            return jsonify({'error': 'Invalid transactions format'}), 400
+        for table_name, file_info in data_to_send.items():
+            file_path = file_info['file_path']
 
-        for transaction in transactions:
-            if 'table_name' not in transaction or 'data' not in transaction:
-                return jsonify({'error': 'Invalid transaction format'}), 400
-            
-            table_name = transaction['table_name'].lower()
-            data = transaction['data']
-            if not isinstance(data, list):
-                return jsonify({'error': 'Invalid data format'}), 400
+            if os.path.exists(file_path):
+                with open(file_path, 'rb') as file:
+                    file_content = file.read().decode('utf-8')
 
-            if table_name == 'departments':
-                department = Department(
-                    id = data[0],
-                    department=data[1]
-                )
-                db.session.add(department)
-            elif table_name == 'jobs':
-                job = Job(
-                    id=data[0], 
-                    job=data[1]
-                )
-                db.session.add(job)
-            elif table_name == 'hired_employees':
-                employee = Employee(
-                    id=data[0],
-                    name=data[1],
-                    datetime=data[2],
-                    department_id=data[3],
-                    job_id=data[4]
-                )
-                db.session.add(employee)
-            else:
-                return jsonify({'error': f'Unsupported table name: {table_name}'}), 400
+                for line in file_content.splitlines():
+                    row_data = line.split(',')
+                    transaction = {
+                        'table_name': table_name,
+                        'data': row_data
+                    }
+                    transactions.append(transaction)
 
-        db.session.commit()
+        transaction_batches = list(chunks(transactions, batch_size))
+
+        for i, listtran in enumerate(transaction_batches):
+            if not isinstance(listtran, list):
+                return jsonify({'error': 'Invalid transactions format'}), 400
+
+            for transaction in listtran:
+                if 'table_name' not in transaction or 'data' not in transaction:
+                    return jsonify({'error': 'Invalid transaction format'}), 400
+                
+                table_name = transaction['table_name'].lower()
+                data = transaction['data']
+                if not isinstance(data, list):
+                    return jsonify({'error': 'Invalid data format'}), 400
+
+                if table_name == 'departments':
+                    department = Department(
+                        id = data[0],
+                        department=data[1]
+                    )
+                    db.session.add(department)
+                elif table_name == 'jobs':
+                    job = Job(
+                        id=data[0], 
+                        job=data[1]
+                    )
+                    db.session.add(job)
+                elif table_name == 'hired_employees':
+                    employee = Employee(
+                        id=data[0],
+                        name=data[1],
+                        datetime=data[2],
+                        department_id=data[3],
+                        job_id=data[4]
+                    )
+                    db.session.add(employee)
+                else:
+                    return jsonify({'error': f'Unsupported table name: {table_name}'}), 400
+
+            db.session.commit()
+            print(f"Lote número {i+1} enviado", file=sys.stdout)
 
         return jsonify({'message': 'Data loaded successfully'}), 200
 
--- consulta.py
+++ consulta.py
@@ -1,46 +1,18 @@
 import requests
-import os
-import json
 
 api_url = 'http://127.0.0.1:5000/load_data'
 
-transactions = []
-batch_size = 1000
-
-def chunks(lst, n):
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
-
 data_to_send = {
-    'departments': {'file_name': 'departments.csv', 'file_path': './load_data/departments.csv'},
-    'jobs': {'file_name': 'jobs.csv', 'file_path': './load_data/jobs.csv'},
-    'hired_employees': {'file_name': 'hired_employees.csv', 'file_path': './load_data/hired_employees.csv'}
+    'departments': {'file_path': './load_data/departments.csv'},
+    'jobs': {'file_path': './load_data/jobs.csv'},
+    'hired_employees': {'file_path': './load_data/hired_employees.csv'}
 }
 
-for table_name, file_info in data_to_send.items():
-    file_name = file_info['file_name']
-    file_path = file_info['file_path']
+payload = {'files': data_to_send}
+response = requests.post(api_url, json=payload)
 
-    if os.path.exists(file_path):
-        with open(file_path, 'rb') as file:
-            file_content = file.read().decode('utf-8')
-
-        for line in file_content.splitlines():
-            row_data = line.split(',')
-            transaction = {
-                'table_name': table_name,
-                'data': row_data
-            }
-            transactions.append(transaction)
-
-transaction_batches = list(chunks(transactions, batch_size))
-
-for i, batch in enumerate(transaction_batches):
-    payload = {'transactions': batch}
-    response = requests.post(api_url, json=payload)
-
-    if response.status_code == 200:
-        print(f"Lote de datos enviado con éxito #{i+1}")
-    else:
-        print(f"Error al enviar el lote de datos {i+1}. Código de estado: {response.status_code}")
-        print(response.text)
+if response.status_code == 200:
+    print("files send.")
+else:
+    print(f"Error with files. cod: {response.status_code}")
+    print(response.text)
